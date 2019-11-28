d5a1a2f5d7704c6aed9d08186a627bb5e2118859
volkzilla <volkzilla@yandex.ru>
2019-11-29 00:49:53 +0700

    Core packages created

---
+++ app/src/main/java/punchclub/lightweight/nsu/ru/android/punchclubapp/modules/feed/FeedActivity.java
@@ -0,0 +1,15 @@
+package punchclub.lightweight.nsu.ru.android.punchclubapp.modules.feed;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import punchclub.lightweight.nsu.ru.android.punchclubapp.R;
+
+public class FeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
+    }
+}
