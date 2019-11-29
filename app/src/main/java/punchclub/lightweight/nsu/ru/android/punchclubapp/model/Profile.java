e17110bb087fb391cb8329ad9d90ea58ed1b3474
volkzilla <volkzilla@yandex.ru>
2019-11-30 00:11:24 +0700

    Post & Profile classes filled
    In this classes contains base functionality and info

--- app/src/main/java/punchclub/lightweight/nsu/ru/android/punchclubapp/model/Profile.java
+++ app/src/main/java/punchclub/lightweight/nsu/ru/android/punchclubapp/model/Profile.java
@@ -1,4 +1,47 @@
 package punchclub.lightweight.nsu.ru.android.punchclubapp.model;
 
 public class Profile {
+    private String name;
+    private int age;
+    private String city;
+    private String sex;
+
+    public Profile(String name, int age, String city, String sex) {
+        this.name = name;
+        this.age = age;
+        this.city = city;
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
 }
