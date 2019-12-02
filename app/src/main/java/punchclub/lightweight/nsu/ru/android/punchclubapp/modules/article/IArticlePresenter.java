294211b73bb36d5cc2ca6cbbabe8c993d790a6fd
volkzilla <volkzilla@yandex.ru>
2019-12-02 23:48:11 +0700

    Article core classes + GetArticle

---
+++ app/src/main/java/punchclub/lightweight/nsu/ru/android/punchclubapp/modules/article/IArticlePresenter.java
@@ -0,0 +1,6 @@
+package punchclub.lightweight.nsu.ru.android.punchclubapp.modules.article;
+
+import punchclub.lightweight.nsu.ru.android.punchclubapp.modules.base.IBasePresenter;
+
+public interface IArticlePresenter extends IBasePresenter {
+}
