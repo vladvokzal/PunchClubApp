160e834af7cd9f51ccca891fb79a6a97eef874b6
volkzilla <volkzilla@yandex.ru>
2019-12-18 00:50:57 +0700

    Update methods in UCTPScheduler

--- app/src/main/java/punchclub/lightweight/nsu/ru/android/punchclubapp/common/UseCaseThreadPoolScheduler.java
+++ app/src/main/java/punchclub/lightweight/nsu/ru/android/punchclubapp/common/UseCaseThreadPoolScheduler.java
@@ -2,10 +2,20 @@ package punchclub.lightweight.nsu.ru.android.punchclubapp.common;
 
 import android.os.Handler;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 public class UseCaseThreadPoolScheduler implements IUseCaseScheduler {
+
+    UseCaseThreadPoolScheduler() {
+        this.threadPoolExecutor = new ThreadPoolExecutor(POOL_SIZE, MAX_POOL_SIZE, TIME_OUT, TimeUnit.SECONDS,
+                new ArrayBlockingQueue<Runnable>(POOL_SIZE));
+    }
+
     @Override
     public void execute(Runnable runnable) {
-        
+        threadPoolExecutor.execute(runnable);
     }
 
     @Override
@@ -27,6 +37,12 @@ public class UseCaseThreadPoolScheduler implements IUseCaseScheduler {
             }
         });
     }
+
+    private static final int POOL_SIZE = 2;
+    private static final int MAX_POOL_SIZE = 2;
+    private static final int TIME_OUT = 2;
+
+    private final ThreadPoolExecutor threadPoolExecutor;
 
     private final Handler handler = new Handler();
 }
