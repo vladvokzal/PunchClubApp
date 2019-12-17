cb96b4df65d418a7141efbe31dd326fc138f8b0a
volkzilla <volkzilla@yandex.ru>
2019-12-18 01:14:37 +0700

    One little test created

--- app/src/test/java/punchclub/lightweight/nsu/ru/android/punchclubapp/ExampleUnitTest.java
+++ app/src/test/java/punchclub/lightweight/nsu/ru/android/punchclubapp/ExampleUnitTest.java
@@ -1,6 +1,7 @@
 package punchclub.lightweight.nsu.ru.android.punchclubapp;
 
 import org.junit.Test;
+import punchclub.lightweight.nsu.ru.android.punchclubapp.model.Profile;
 
 import static org.junit.Assert.*;
 
@@ -13,5 +14,14 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void profileCreation_isCorrect(){
+        Profile profile = new Profile("Goga", 25, "NSO", "m");
+        assertEquals(25, profile.getAge());
+        assertEquals("Goga", profile.getName());
+        assertEquals("NSO", profile.getCity());
+        assertEquals("m", profile.getSex());
     }
 }
