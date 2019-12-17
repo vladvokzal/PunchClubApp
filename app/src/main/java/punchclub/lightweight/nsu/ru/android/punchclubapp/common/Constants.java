3c9c4f7d06a3f8414ba838742c0051184e1918af
volkzilla <volkzilla@yandex.ru>
2019-12-17 23:33:55 +0700

    One constant created

--- app/src/main/java/punchclub/lightweight/nsu/ru/android/punchclubapp/common/Constants.java
+++ app/src/main/java/punchclub/lightweight/nsu/ru/android/punchclubapp/common/Constants.java
@@ -2,4 +2,6 @@ package punchclub.lightweight.nsu.ru.android.punchclubapp.common;
 
 public class Constants {
     public static final String STACK_TRACE_LOG_TAG = "STACK_TRACE_LOG_TAG";
+
+    public static final String GLOBAL_ANSWER = "42";
 }
