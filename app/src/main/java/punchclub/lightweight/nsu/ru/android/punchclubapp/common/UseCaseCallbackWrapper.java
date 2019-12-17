b0327b2405cf842d83cecada87868372f2068bc2
volkzilla <volkzilla@yandex.ru>
2019-12-18 00:57:38 +0700

    Filled wrapper for UC callback

--- app/src/main/java/punchclub/lightweight/nsu/ru/android/punchclubapp/common/UseCaseCallbackWrapper.java
+++ app/src/main/java/punchclub/lightweight/nsu/ru/android/punchclubapp/common/UseCaseCallbackWrapper.java
@@ -1,4 +1,21 @@
 package punchclub.lightweight.nsu.ru.android.punchclubapp.common;
 
-public class UseCaseCallbackWrapper {
+public final class UseCaseCallbackWrapper<T extends UseCase.ResponseValues> implements IUseCaseCallback<T> {
+    @Override
+    public void onSuccess(T response) {
+        useCaseHandler.notifyResponse(response, callback);
+    }
+
+    @Override
+    public void onError() {
+        useCaseHandler.notifyError(callback);
+    }
+
+    UseCaseCallbackWrapper(IUseCaseCallback<T> callback, UseCaseHandler useCaseHandler){
+        this.callback = callback;
+        this.useCaseHandler = useCaseHandler;
+    }
+
+    private final IUseCaseCallback<T> callback;
+    private final UseCaseHandler useCaseHandler;
 }
