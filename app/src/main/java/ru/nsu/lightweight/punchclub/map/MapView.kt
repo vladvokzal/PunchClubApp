5270d6595e4f6cb8e568fb7ca8e954a1393d6bb4
Mikov Ilya <mikov08.08@gmail.com>
2019-12-21 01:55:04 +0700

    Update map screen

--- app/src/main/java/ru/nsu/lightweight/punchclub/map/MapView.kt
+++ app/src/main/java/ru/nsu/lightweight/punchclub/map/MapView.kt
@@ -1,5 +1,9 @@
 package ru.nsu.lightweight.punchclub.map
 
 import com.arellomobile.mvp.MvpView
+import ru.nsu.lightweight.punchclub.dataBase.fighter.Fighter
 
-interface MapView : MvpView, Map<BaseMarker>
+interface MapView : MvpView, Map<BaseMarker> {
+
+    fun setFighters(fighters: List<Fighter>)
+}
