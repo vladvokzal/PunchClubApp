76a2c374b6a558cf02226484b16bf81d9c90af50
Mikov Ilya <mikov08.08@gmail.com>
2019-12-21 01:55:16 +0700

    Update profile screen

--- app/src/main/java/ru/nsu/lightweight/punchclub/profile/ProfileActivity.kt
+++ app/src/main/java/ru/nsu/lightweight/punchclub/profile/ProfileActivity.kt
@@ -3,14 +3,24 @@ package ru.nsu.lightweight.punchclub.profile
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.arellomobile.mvp.MvpAppCompatActivity
+import com.arellomobile.mvp.presenter.InjectPresenter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import ru.nsu.lightweight.punchclub.R
+import ru.nsu.lightweight.punchclub.dataBase.fighter.Fighter
 import ru.nsu.lightweight.punchclub.databinding.ActivityProfileBinding
 import ru.nsu.lightweight.punchclub.utils.Animation
 import ru.nsu.lightweight.punchclub.utils.RouterUtil
 
 class ProfileActivity : MvpAppCompatActivity(), ProfileView {
 
+    @InjectPresenter
+    lateinit var presenter: ProfilePresenter
     private lateinit var binding: ActivityProfileBinding
+
+    private val requestOptions = RequestOptions()
+        .centerCrop()
+        .dontAnimate()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +29,20 @@ class ProfileActivity : MvpAppCompatActivity(), ProfileView {
         setContentView(binding.root)
     }
 
+    override fun showFighter(fighter: Fighter) {
+        binding.name.text = fighter.name
+        binding.age.text = fighter.age.toString()
+        binding.description.text = fighter.description
+        Glide.with(binding.avatar)
+            .load(fighter.avatarUrl)
+            .apply(requestOptions)
+            .into(binding.avatar)
+    }
+
     private fun initUI() {
+        val id = intent.getLongExtra("ARG_ID_FIGHTER", 0)
+        presenter.loadFighterProfile(id)
+
         binding.toolbar.back.setOnClickListener {
             onBackPressed()
         }
