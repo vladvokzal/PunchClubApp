b8f4b860c62fde63a98ac655902c7325c524468b
Mikov Ilya <mikov08.08@gmail.com>
2019-12-21 01:53:19 +0700

    Update base marker

--- app/src/main/java/ru/nsu/lightweight/punchclub/map/BaseMarker.kt
+++ app/src/main/java/ru/nsu/lightweight/punchclub/map/BaseMarker.kt
@@ -2,10 +2,15 @@ package ru.nsu.lightweight.punchclub.map
 
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.clustering.ClusterItem
+import ru.nsu.lightweight.punchclub.dataBase.fighter.Fighter
 
 open class BaseMarker(
     val id: Long,
-    val latLng: LatLng
+    val latLng: LatLng,
+    val avatarUrl: String,
+    val name: String,
+    val description: String,
+    val age: Int
 ) : ClusterItem {
 
     override fun getTitle(): String {
@@ -19,4 +24,14 @@ open class BaseMarker(
     override fun getPosition(): LatLng {
         return latLng
     }
+
+    fun toFighter() = Fighter(
+        id = id,
+        lat = latLng.latitude,
+        lng = latLng.longitude,
+        avatarUrl = avatarUrl,
+        name = name,
+        description = description,
+        age = age
+    )
 }
